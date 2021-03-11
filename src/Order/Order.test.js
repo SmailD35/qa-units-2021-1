05902a92c5c752980bf47526d14c31f60ac814fe
Aleksandr Dergachev <smaild-m@yandex.ru>
2021-03-11 21:19:00 +0300

    Добавил тесты

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -1,8 +1,81 @@
+jest.mock('../utils/getDate');
+
 import React from 'react'
+import {shallow, configure} from 'enzyme';
+import {shallowToJson} from 'enzyme-to-json';
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Order from './Order';
+import {getDate} from "../utils/getDate";
+Enzyme.configure({ adapter: new Adapter() })
 
-describe('Order.js', () => {
-  it('some test', () => {
-
-  });
+afterEach(() => {
+  jest.clearAllMocks();
 });
 
+describe('Order.js', () => {
+  it('order without items', () => {
+    getDate.mockReturnValue('14 марта, чт, 2019 год');
+
+    const order = {
+      date: 1552585550000,
+      shop: 'Эльдоградо'
+    };
+
+    const output = shallow(
+        <Order
+            order={order}
+        />
+    );
+    expect(shallowToJson(output)).toMatchSnapshot();
+    expect(getDate).toBeCalledTimes(1);
+  });
+
+  it('order with items', () => {
+    getDate.mockReturnValue('13 марта, ср, 2019 год');
+
+    const order = {
+      date: 1552585550000,
+      shop: 'Lamodник.ru',
+      items: [
+        'Жакет - BOREAL5',
+        'Miss Gabby Костюм',
+        'Ostin перчатки мужские',
+        'Zara худи роз.',
+      ]
+    };
+
+    const output = shallow(
+        <Order
+            order={order}
+        />
+    );
+    expect(shallowToJson(output)).toMatchSnapshot();
+    expect(getDate).toBeCalledTimes(1);
+  });
+
+  it('order without date and shop', () => {
+    const order = {
+      items: [
+        'Жакет - BOREAL5',
+        'Miss Gabby Костюм',
+        'Ostin перчатки мужские',
+        'Zara худи роз.',
+      ]
+    };
+
+    const output = shallow(
+        <Order
+            order={order}
+        />
+    );
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+
+  it('order is undefined', () => {
+    const output = shallow(
+        <Order/>
+    );
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+});
--- src/utils/sortOrders.test.js
+++ src/utils/sortOrders.test.js
@@ -1,9 +1,28 @@
 import React from 'react'
-import {sortByItemCount} from './sortOrders';
+import {sortByItemCount, sortOrders} from './sortOrders';
+import {sortByDate} from './sortOrders';
 
 describe('sortByItemCount function', () => {
 	it('orders are null', () => {
 		const result = sortByItemCount(null, null);
+		expect(result).toEqual(0);
+	});
+
+	it('orders are undefined', () => {
+		const result = sortByItemCount(undefined, undefined);
+		expect(result).toEqual(0);
+	});
+
+	it('orders have no items', () => {
+		const order1 = {
+			date: 1544356800000,
+		};
+
+		const order2 = {
+			date: 1544356800000,
+		};
+
+		const result = sortByItemCount(order1, order2);
 		expect(result).toEqual(0);
 	});
 
@@ -20,5 +39,133 @@ describe('sortByItemCount function', () => {
 
 		expect(result).toBe(0);
 	});
+
+	it('first order items count is greater then the second ones', () => {
+		const order1 = {
+			items: ['item1', 'item2', 'item3'],
+		};
+
+		const order2 = {
+			items: ['1', '2'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(1);
+	});
+
+	it('second order items count is greater then the first ones', () => {
+		const order1 = {
+			items: ['item1', 'item2'],
+		};
+
+		const order2 = {
+			items: ['1', '2', '3'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(-1);
+	});
 });
 
+describe('sortByDate function', () => {
+	it('orders are null', () => {
+		const result = sortByDate(null, null);
+		expect(result).toEqual(0);
+	});
+
+	it('orders are undefined', () => {
+		const result = sortByDate(undefined, undefined);
+		expect(result).toEqual(0);
+	});
+
+	it('orders have no date', () => {
+		const order1 = {
+			items: ['item1', 'item2'],
+		};
+
+		const order2 = {
+			items: ['item1', 'item2'],
+		};
+
+		const result = sortByDate(order1, order2);
+		expect(result).toEqual(0);
+	});
+
+	it('same orders date', () => {
+		const order1 = {
+			date: 1544356800000,
+		};
+
+		const order2 = {
+			date: 1544356800000,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('first order date is greater then the second ones', () => {
+		const order1 = {
+			date: 1552585550000,
+		};
+
+		const order2 = {
+			date: 1552481120000,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(-1);
+	});
+
+	it('second order date is greater then the first ones', () => {
+		const order1 = {
+			date: 1552481120000,
+		};
+
+		const order2 = {
+			date: 1552585550000,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(1);
+	});
+});
+
+describe('sortOrders function', () => {
+	it('orders are null', () => {
+		const result = sortOrders(null, sortByItemCount);
+		expect(result).toEqual(undefined);
+	});
+
+	it('sortFunction is not a function', () => {
+		const orders = [
+			{
+				id: 124,
+				date: 1552481120000,
+				shop: 'Lamodник.ru',
+				items: [
+					'Жакет - BOREAL5',
+					'Miss Gabby Костюм',
+					'Ostin перчатки мужские',
+					'Zara худи роз.',
+				]
+			},
+			{
+				id: 126,
+				date: 1552585550000,
+				shop: 'Эльдоградо',
+				items: [
+					'Ноутбук Apple MacBook Air 13.3" (MQD32RU/A)',
+					'Игровая приставка Sony PlayStation 4 Pro 1TB Black (CUH-7208B)',
+				]
+			},
+		]
+		const result = sortOrders(orders, 1);
+		expect(result).toEqual(undefined);
+	});
+});
